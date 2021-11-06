520c529ad9f87cc88469d3e357a11dfcae98159c
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-11-06 11:20:14 -0400

    Update NumberOfOnes.java

--- bit-manipulation/NumberOfOnes.java
+++ bit-manipulation/NumberOfOnes.java
@@ -17,6 +17,23 @@ public class NumberOfOnes {
         }
         return count;
     }
+    
+    public static int numOfOnesLookupTable(int num) {
+        int[] table = new int[256];
+
+        table[0] = 0;
+        for (int i = 1; i < 256; i++) {
+            table[i] = (i & 1) + table[i >> 1];
+        }
+
+        int res = 0;
+        for (int i = 0; i < 4; i++) {
+            res += table[n & 0xff];
+            n >>= 8;
+        }
+
+        return res;
+    }
 
     public static void main(String[] args) {
         System.out.println(numOfOnesBitManipulation(6));
