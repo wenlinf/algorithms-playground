ed1bde0b942f1f87f95d2ab79edbdb3c2b3bd8b8
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-11-07 11:57:56 -0500

    Create MissingNumber.java

---
+++ bit-manipulation/MissingNumber.java
@@ -0,0 +1,31 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class MissingNumber {
+    private static int helper(int[] nums) {
+        int sum = (0 + nums.length) * (nums.length + 1) / 2;
+        int total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+        return sum - total;
+    }
+    
+    private static int helperWithSet(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        for (int i = 0; i <= nums.length; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+        System.out.println("Missing element in the array is " + helper(nums));
+    }
+}
