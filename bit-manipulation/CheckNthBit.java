1abdd9d3e4a6622cee3fa22d6282b7d24af9cf85
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-11-08 07:51:36 -0500

    Update CheckNthBit.java
    another solution using right shift

--- bit-manipulation/CheckNthBit.java
+++ bit-manipulation/CheckNthBit.java
@@ -1,13 +1,23 @@
 
 public class CheckNthBit {
     public static boolean checkNthBit(int num, int index) {
+        // using left shift
         return (num & 1 << index) != 0;
     }
 
-
+    public static boolean checkKthBitSet(int n, int k) {
+        // using right shift
+        return ((n >> (k - 1)) & 1) == 1;
+    }
 
     public static void main(String[] args) {
         System.out.println(checkNthBit(5, 1));
+        
+        System.out.println("n = 5, k = 3 : " + checkKthBitSet(5, 3));
+        System.out.println("------------");
+        System.out.println("n = 10, k = 2 : " + checkKthBitSet(10, 2));
+        System.out.println("------------");
+        System.out.println("n = 10, k = 1 : " + checkKthBitSet(10, 1));
     }
 
 }
