c3d23a4ec6e6f39e5467bf9dfbee86a377969dea
wenlinf <42773608+wenlinf@users.noreply.github.com>
2021-11-18 12:59:19 -0500

    Update InsertionSort.java
    updated formatting

---
+++ sort/InsertionSort.java
@@ -0,0 +1,22 @@
+public class InsertionSort {
+    public static int[] insertion(int[] arr) {
+        int temp;
+        for (int i = 1; i < arr.length; i++) {
+            temp = arr[i];
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] > temp) {
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] ints = insertion(new int[]{3, 2, 6, 5, 7, 6, 9, 2, 4, 1});
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(ints[i]);
+        }
+    }
+}
